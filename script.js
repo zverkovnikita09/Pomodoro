76ae33a173a71b5d431509dfe07dc64b97564f79
unknown <zverkovnikita09@gmail.com>
2023-06-22 00:56:56 +0300

    create 3 timer types; styling

--- script.js
+++ script.js
@@ -1,63 +1,126 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const minutesHtml = document.getElementById('minutes');
-  const secondsHtml = document.getElementById('seconds');
-  const startBtn = document.getElementById('start');
+  const container = document.querySelector('.container');
+  const minutesHtml = document.querySelector('.minutes');
+  const secondsHtml = document.querySelector('.seconds');
+  const startBtn = document.querySelector('.startBtn');
+  const nextStepButton = document.querySelector('.next-step');
+  const settingsButton = document.querySelector('.settings-button');
+  const settings = document.querySelector('.settings')
+  const tasksBlock = document.querySelector('.tasks-container')
 
-  let minutes = 1;
-  let seconds = 0;
+  const tasks = [{
+    id: 1,
+    task: 'babababa',
+    isDone: true,
+    pomodoros: 0
+  }, {
+    id: 2,
+    task: 'bebebebe',
+    isDone: false,
+    pomodoros: 0
+  }]
+
+  let initialTimerValues = {
+    workTime: 25 * 60,
+    shortBreakTime: 5 * 60,
+    LongBreakTime: 15 * 60,
+  };
+
+  let time = initialTimerValues.workTime; //in seconds
   let interval;
   let isInterval;
-  let period = 1;
+  let iteration = 1;
 
-  const renderMinutes = () => {
-    let minutesString = String(minutes);
-    if (minutesString.length < 2) {
-      minutesString = '0' + minutesString
+  const renderTime = () => {
+    let minutes = String(Math.floor(time / 60));
+    let seconds = String(time - minutes * 60)
+    if (minutes.length < 2) {
+      minutes = '0' + minutes
     }
-    minutesHtml.innerHTML = minutesString;
+    if (seconds.length < 2) {
+      seconds = '0' + seconds
+    }
+    minutesHtml.innerHTML = minutes;
+    secondsHtml.innerHTML = seconds;
   }
 
-  const renderSeconds = () => {
-    let secondsString = String(seconds);
-    if (secondsString.length < 2) {
-      secondsString = '0' + secondsString
+  const switchTimer = (timerType) => {
+    if (timerType === 'workTime') {
+      time = initialTimerValues.workTime;
+      container.classList = 'container work'
     }
-    secondsHtml.innerHTML = secondsString;
+    if (timerType === 'shortBreak') {
+      time = initialTimerValues.shortBreakTime;
+      container.classList = 'container short-break'
+    }
+    if (timerType === 'longBreak') {
+      time = initialTimerValues.LongBreakTime;
+      container.classList = 'container long-break'
+    }
+    renderTime();
   }
 
-  const updateTimer = () => {
-    if (seconds) {
-      seconds--;
-      renderSeconds();
-    }
-    else {
-      if (minutes) {
-        seconds = 59;
-        minutes--;
-        renderMinutes();
-        renderSeconds();
-      }
-      else clearInterval(interval);
-    }
-  }
-
-  const startTimer = () => {
-    startBtn.innerHTML = 'Пауза'
-    isInterval = true;
-    interval = setInterval(updateTimer, 1000)
-  }
-
-  const pauseTimer = () => {
+  const stopTimer = () => {
     startBtn.innerHTML = 'Старт'
     isInterval = false;
     clearInterval(interval)
   }
 
-  const toggleTimer = () => {
-    isInterval ? pauseTimer() : startTimer()
+  const nextPeriod = () => {
+    iteration++;
+    stopTimer();
+    if ((iteration / 2) % 4 === 0) {
+      switchTimer('longBreak')
+      return
+    }
+    if (iteration % 2 === 0) {
+      switchTimer('shortBreak')
+      return
+    }
+    switchTimer('workTime')
   }
 
-  renderMinutes();
-  renderSeconds();
+  const startTimer = () => {
+    startBtn.innerHTML = 'Пауза'
+    isInterval = true;
+    interval = setInterval(() => {
+      if (!time) {
+        nextPeriod()
+        return
+      }
+
+      time--;
+      renderTime();
+    }, 1000)
+  }
+
+  const toggleTimer = () => {
+    isInterval ? stopTimer() : startTimer()
+  }
+
+  const renderTasks = () => {
+    tasks.forEach(task=>{
+      tasksBlock.innerHTML += `<li class="task ${task.isDone ? 'done' : ''}">
+      ${task.task}
+      </li>`
+    })
+  }
+
+  const toggleSettings = () => {
+    settings.classList.toggle('open');
+    window.addEventListener('click', closeSettingByOutsideClick)
+  }
+
+  const closeSettingByOutsideClick = (e) => {
+    if (e.target !== settings && e.target !== settingsButton) {
+      settings.classList.remove('open')
+      window.removeEventListener('click', closeSettingByOutsideClick)
+    }
+  }
+
   startBtn.addEventListener('click', toggleTimer);
+  nextStepButton.addEventListener('click', nextPeriod);
+  settingsButton.addEventListener('click', toggleSettings)
+  renderTime();
+  renderTasks();
 })
